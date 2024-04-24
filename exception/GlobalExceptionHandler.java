cd8caaef3ac2086232fb4f31f9440c5a2419ba58
MahmoudAhmed <59095283+mar4419440@users.noreply.github.com>
2024-04-24 17:34:24 +0200

    Create GlobalExceptionHandler.java

---
+++ exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class GlobalExceptionHandler {
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(ex.getBindingResult().getAllErrors());
+    }
+}
