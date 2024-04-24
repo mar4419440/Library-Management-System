22f46e7142c4e4e18e9557057035e3d5dfb89966
MahmoudAhmed <59095283+mar4419440@users.noreply.github.com>
2024-04-24 17:43:32 +0200

    Create LibraryMangmentSysytemApplicationTests.java

---
+++ Test Packages/com.example.Library.mangment.Sysytem/LibraryMangmentSysytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Library.mangment.Sysytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMangmentSysytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
